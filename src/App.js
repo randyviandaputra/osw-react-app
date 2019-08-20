d281e2b36ff10c876e014afa691429377a756bf0
suschevsky <suschevsky.sv@gmail.com>
2019-08-20 10:05:04 +0500

    Add login stub

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -6,6 +6,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import {makeStyles} from "@material-ui/styles";
+import {Container} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   app: {
@@ -19,8 +20,9 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function App() {
+const App = () => {
   const classes = useStyles();
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className={classes.app}>
       <AppBar position="static">
@@ -31,11 +33,14 @@ function App() {
           <Typography variant="h6" className={classes.title}>
             osw-react-app
           </Typography>
-          <Button className={classes.loginButton}>Login</Button>
+          <Button className={classes.loginButton} onClick={() => setLoggedIn(!loggedIn)}>Login</Button>
         </Toolbar>
       </AppBar>
+      <Container>
+        {loggedIn ? <h1>Logged in</h1>: <h1>Please log in</h1>}
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
